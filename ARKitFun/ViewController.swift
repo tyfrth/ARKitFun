28d1d69eae5bd7899b5d7cd943a3e955defbd029
tyfrth <tyfrith@gmail.com>
2018-06-21 13:28:53 -0700

    purple box

--- ARKitFun/ViewController.swift
+++ ARKitFun/ViewController.swift
@@ -22,15 +22,23 @@ class ViewController: UIViewController {
         self.sceneView.session.run(configuration)
         
         //add debug options
-        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
-      
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        
+        //create and add node
+        let node = SCNNode()
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.purple
+        node.position = SCNVector3(0,0,-0.5)
+        node.scale = SCNVector3(1.5, 1.5, 1.5)
+        
+        self.sceneView.scene.rootNode.addChildNode(node)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
+
 
